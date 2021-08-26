fb376c1957a60c9fa5f3cfc8922a859d579e7aca
Ogneslav818 <ogneslav18@gmail.com>
2021-08-26 20:05:54 +0300

    My first homework

---
+++ Zadanie.py
@@ -0,0 +1,7 @@
+name = input('Введите ваше имя: ')
+surname = input('Введите вашу фамилию: ')
+age = input('Сколько вам лет: ')
+city = input('Из какого вы города ')
+print(name, surname, age, city)
+
+
